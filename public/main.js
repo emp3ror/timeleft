11bf9df26b935ea665d9fbeda3db6225cef47779
mjt <thapa.manish16@gmail.com>
2016-12-31 21:29:41 +0545

    formatting dates

--- public/main.js
+++ public/main.js
@@ -2,7 +2,21 @@ $(function () {
 
 	var socket = io();
 	socket.on('time',function (msg) {
-		$('.time').text(msg);
-		console.log(new Date(msg));
+		/*$('.time').html(
+"<span>"+msg.day+" day </span>"+
+"<span>"+msg.hrs+" hrs </span>"+
+"<span>"+msg.min+" min </span>"+
+"<span>"+msg.sec+" sec </span>");
+	*/
+	if (msg.day !== 0) {
+		$('.time .day').text(msg.day+" day(s)");
+	}
+
+	if (msg.hrs !== 0 && msg.day !== 0) {
+		$('.time .hours').text(msg.hrs+" : ");
+	}
+
+	$('.time .minute').text(msg.min+" : ");
+	$('.time .second').text(msg.sec+" ");
 	})
 });
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ http.listen(3000,function () {
 	console.log('listen');
 })
 console.log(Date.now());
-var time = Math.floor((Date.now()+5000)/1000);
+var time = Math.floor((Date.now()+1476005000)/1000);
 
 io.on('connection',function (socket) {
 	console.log('who connected now?? ');
@@ -32,7 +32,9 @@ io.on('connection',function (socket) {
 	function init() {
 		setTimeout(function () {
 			var diffTime = time- Math.floor(Date.now()/1000);
-			io.emit("time", diffTime);
+			tmObj = timehs(diffTime);
+			console.log(tmObj);
+			io.emit("time", tmObj);
 			if (diffTime > 0) {
 				init();
 			}
@@ -45,3 +47,29 @@ io.on('connection',function (socket) {
 		console.log('damm s/he(*) got disconnected');
 	})
 })
+
+function timehs(time) {
+	var sec =0,min=0,hrs=0,day=0
+	sec = time;
+	if (sec > 59) {
+		min = Math.floor(sec/60);
+		sec = sec - min*60;
+	}
+
+	if (min > 59) {
+		hrs = Math.floor(min/60);
+		min = min - hrs*60;
+		// sec = sec - min*60;	
+	}
+
+	if (hrs > 23) {
+		day = Math.floor(hrs/24);
+		hrs = hrs - day*24;
+	}
+
+	return {day : day,
+		hrs : hrs,
+		min : min,
+		sec : sec
+	}
+}
